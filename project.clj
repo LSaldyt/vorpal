aa9a94db538cae24af0c0974a14a0ad9e135f75a
LSaldyt <lucassaldyt@gmail.com>
2017-12-27 20:39:26 -0700

    WIP

--- project.clj
+++ project.clj
@@ -1,21 +1,14 @@
-(defproject vorpal "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  ;;:license {:name "Eclipse Public License v1.0"
-            ;;:url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]
+(defproject clojure-getting-started "1.0.0-SNAPSHOT"
+  :description "Demo Clojure web app"
+  :url "http://clojure-getting-started.herokuapp.com"
+  :license {:name "Eclipse Public License v1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.0.0"]]
-  ;;:plugins [[lein-ring "0.9.7"]]
+  :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
-  :ring {:handler vorpal.handler/app}
   :hooks [environ.leiningen.hooks]
   :uberjar-name "clojure-getting-started-standalone.jar"
   :profiles {:production {:env {:production true}}})
-;  :profiles
-;  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-;                        [ring/ring-mock "0.3.0"]]}})
-;
