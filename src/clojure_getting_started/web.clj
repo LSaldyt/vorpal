5cf4525e05669866e931045aca4b65dc51f4a0f6
LSaldyt <lucassaldyt@gmail.com>
2017-12-27 22:11:15 -0700

    WIP

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -4,8 +4,7 @@
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
-            [environ.core :refer [env]
-            [camel-snake-kebab.core :as kebab]]))
+            [environ.core :refer [env]]))
 
 (defn splash []
   {:status 200
