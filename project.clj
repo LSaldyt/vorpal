4b16d7c59210100d9cee2296727de60dc5676a79
LSaldyt <lucassaldyt@gmail.com>
2017-12-27 20:37:06 -0700

    Heroku test

--- project.clj
+++ project.clj
@@ -1,12 +1,21 @@
 (defproject vorpal "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  ;;:license {:name "Eclipse Public License v1.0"
+            ;;:url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
-  :plugins [[lein-ring "0.9.7"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [environ "1.0.0"]]
+  ;;:plugins [[lein-ring "0.9.7"]]
+  :plugins [[environ/environ.lein "0.3.1"]]
   :ring {:handler vorpal.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "clojure-getting-started-standalone.jar"
+  :profiles {:production {:env {:production true}}})
+;  :profiles
+;  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+;                        [ring/ring-mock "0.3.0"]]}})
+;
--- src/vorpal/handler.clj
+++ src/vorpal/handler.clj
@@ -1,11 +1,26 @@
-(ns vorpal.handler
-  (:require [compojure.core :refer :all]
+(ns clojure-getting-started.web
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [clojure.java.io :as io]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello Lucas!")
-  (route/not-found "Not Found"))
+(defn splash []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello from Heroku"})
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(defroutes app
+  (GET "/" []
+       (splash))
+  (ANY "*" []
+       (route/not-found (slurp (io/resource "404.html")))))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
+
+;; For interactive development:
+;; (.stop server)
+;; (def server (-main))
