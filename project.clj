60f0427e73561cb72be4895fac6a7c5959278a4e
LSaldyt <lucassaldyt@gmail.com>
2018-01-02 15:12:29 -0700

    Makes project runnable locally

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-getting-started "1.0.0-SNAPSHOT"
+(defproject vorpal "1.0.0-SNAPSHOT"
   :description "Demo Clojure web app"
   :url "http://clojure-getting-started.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
@@ -13,5 +13,6 @@
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
-  :uberjar-name "clojure-getting-started-standalone.jar"
+  :uberjar-name "vorpal-standalone.jar"
+  :main vorpal.web
   :profiles {:production {:env {:production true}}})
